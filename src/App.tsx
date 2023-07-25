dc119d149e2cdb84dfbc077b1f6ba6680c86b2af
devDohyun <dohyun10101@gmail.com>
2023-07-26 00:48:19 +0900

    Feat: 기본 페이지에서 button을 render합니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import styled from '@emotion/styled'
+import Button from '@/components/common/Button'
 
 const App = () => {
   return (
     <StyledContainer>
       <strong>안녕, 세상!</strong>
+      <Button type="button">눌러보기</Button>
     </StyledContainer>
   )
 }
