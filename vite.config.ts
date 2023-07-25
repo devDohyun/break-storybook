55b50ce71e3b5469b59dbbfbcdfa7291dbf07296
devDohyun <dohyun10101@gmail.com>
2023-07-26 00:48:44 +0900

    Chore: ts, vite path alias 설정

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+  },
+})
