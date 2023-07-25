9e2055f505a70bb0b7245a9cbdcf9adeb7c8bdeb
devDohyun <dohyun10101@gmail.com>
2023-07-26 00:47:47 +0900

    Add: 기본 button component 추가

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,15 @@
+import { colors } from '@/styles/palette'
+import styled from '@emotion/styled'
+
+const CTAButton = styled.button`
+  padding: 8px 16px;
+
+  color: #ffffff;
+  font-size: 18px;
+
+  background-color: ${colors.primary};
+  border: none;
+  border-radius: 8px;
+`
+
+export default CTAButton
